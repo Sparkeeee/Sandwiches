b90e1ba9aacb5c32590b43ec24927a4300b7e402
Carl aka Gnomeski <carlraysparks@gmail.com>
2024-02-19 12:28:58 +0000

    Get latest stock data row from spreadsheet

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -69,6 +70,26 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data():
+    """
+    Compare sales with stock and calculate surplus of each sandwich type.
+    Surplus is stock minus sales.
+    Positive surplus = waste.
+    Negative = number of sandwiches made on demand.
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data()
+
+print("Welcome to Love Sandwiches data automation")
+main()
